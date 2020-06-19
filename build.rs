f975037fb50620a45d51e1f221263a014db2c432
Martin Suesskraut <22873554+msuesskraut@users.noreply.github.com>
2020-06-19 22:31:10 +0200

    build hunspell-sys also on Windows - msvc does not support autotools &   hunspell's build scripts itself do not play well with msvc - just compile the hunspell-1.7.lib with cc "by hand" - keep build without msvc as it is

--- build.rs
+++ build.rs
@@ -2,7 +2,46 @@ use std::env;
 use std::error::Error;
 use std::path::PathBuf;
 
+#[cfg(not(target_env = "msvc"))]
 use autotools;
+
+#[cfg(target_env = "msvc")]
+use cc;
+
+#[cfg(target_env = "msvc")]
+fn build_hunspell(builder: bindgen::Builder) -> bindgen::Builder {
+    cc::Build::new()
+        .file("vendor/src/hunspell/affentry.cxx")
+        .file("vendor/src/hunspell/affixmgr.cxx")
+        .file("vendor/src/hunspell/csutil.cxx")
+        .file("vendor/src/hunspell/filemgr.cxx")
+        .file("vendor/src/hunspell/hashmgr.cxx")
+        .file("vendor/src/hunspell/hunspell.cxx")
+        .file("vendor/src/hunspell/hunzip.cxx")
+        .file("vendor/src/hunspell/phonet.cxx")
+        .file("vendor/src/hunspell/replist.cxx")
+        .file("vendor/src/hunspell/suggestmgr.cxx")
+        .define("BUILDING_LIBHUNSPELL", "1")
+        .cpp(true)
+        .compile("hunspell-1.7");
+
+    builder.clang_arg(format!("-I{}", "vendor/src"))
+}
+
+#[cfg(not(target_env = "msvc"))]
+fn build_hunspell(builder: bindgen::Builder) -> bindgen::Builder {
+    let dst = autotools::Config::new("vendor")
+        .reconf("-ivf")
+        .cxxflag("-fPIC")
+        .build();
+
+    println!(
+        "cargo:rustc-link-search=native={}",
+        dst.join("lib").display()
+    );
+
+    builder.clang_arg(format!("-I{}", dst.join("include").display()))
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut builder = bindgen::Builder::default();
@@ -15,23 +54,16 @@ fn main() -> Result<(), Box<dyn Error>> {
         None
     };
 
-    if pkg_config::probe_library("hunspell").is_err() {
-        let dst = autotools::Config::new("vendor")
-            .reconf("-ivf")
-            .cxxflag("-fPIC")
-            .build();
 
-        println!(
-            "cargo:rustc-link-search=native={}",
-            dst.join("lib").display()
-        );
+    if pkg_config::probe_library("hunspell").is_err() {
+
         println!("cargo:rustc-link-lib=static=hunspell-1.7");
 
         if let Some(link) = libcpp {
             println!("cargo:rustc-link-lib={}", link);
         }
 
-        builder = builder.clang_arg(format!("-I{}", dst.join("include").display()));
+        builder = build_hunspell(builder);
     }
 
     let bindings = builder
